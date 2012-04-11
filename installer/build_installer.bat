9054a4e263dfc418cd1e29101bdd9cb5a763e020
XhmikosR <xhmikosr@users.sourceforge.net>
2012-04-11 08:55:36 +0000

    fix build_installer.bat to work when InnoSetupPath doesn't exist
    git-svn-id: https://vp8vfw.googlecode.com/svn/trunk@18 bb17a04b-3615-95fa-da3c-e23d161940f7

--- installer/build_installer.bat
+++ installer/build_installer.bat
@@ -4,18 +4,12 @@ CD /D %~dp0
 
 REM You can set here the Inno Setup path if for example you have Inno Setup Unicode
 REM installed and you want to use the ANSI Inno Setup which is in another location
-SET "InnoSetupPath=H:\progs\thirdparty\isetup"
+IF NOT DEFINED InnoSetupPath SET "InnoSetupPath=H:\progs\thirdparty\isetup"
 
-rem Check the building environment
+REM Check the building environment
 CALL :SubDetectInnoSetup
 
-IF NOT EXIST %InnoSetupPath% (
-  ECHO. & ECHO.
-  CALL :SUBMSG "ERROR" "Inno Setup not found!"
-  GOTO END
-)
-
-CALL :SubInno
+CALL :SubInno %1
 
 
 :END
@@ -27,46 +21,43 @@ EXIT /B
 :SubInno
 ECHO.
 TITLE Building vfw_vs2010 installer...
-"%InnoSetupPath%\iscc.exe" /Q "setup.iss"
-IF %ERRORLEVEL% NEQ 0 (
-  CALL :SUBMSG "ERROR" "Build failed!
-) ELSE (
-  CALL :SUBMSG "INFO" "Installer compiled successfully!
-)
+"%InnoSetupPath%\ISCC.exe" /Q "setup.iss"
+IF %ERRORLEVEL% NEQ 0 (GOTO EndWithError) ELSE (ECHO Installer compiled successfully!)
 EXIT /B
 
 
 :SubDetectInnoSetup
-REM Detect if we are running on 64bit WIN and use Wow6432Node, and set the path
-REM of Inno Setup accordingly
+REM Detect if we are running on 64bit Windows and use Wow6432Node since Inno Setup is
+REM a 32-bit application, and set the registry key of Inno Setup accordingly
 IF DEFINED PROGRAMFILES(x86) (
   SET "U_=HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
 ) ELSE (
   SET "U_=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
 )
 
-IF NOT DEFINED InnoSetupPath (
+IF DEFINED InnoSetupPath IF NOT EXIST "%InnoSetupPath%" (
+  ECHO "%InnoSetupPath%" wasn't found on this machine! I will try to detect Inno Setup's path from the registry...
+)
+
+IF NOT EXIST "%InnoSetupPath%" (
   FOR /F "delims=" %%a IN (
     'REG QUERY "%U_%\Inno Setup 5_is1" /v "Inno Setup: App Path"2^>Nul^|FIND "REG_"') DO (
     SET "InnoSetupPath=%%a" & CALL :SubInnoSetupPath %%InnoSetupPath:*Z=%%)
 )
 
-IF NOT EXIST "%InnoSetupPath%" ECHO Inno Setup wasn't found! & GOTO END
+IF NOT EXIST "%InnoSetupPath%" ECHO Inno Setup wasn't found! & GOTO EndWithError
 EXIT /B
 
 
 :SubInnoSetupPath
-SET InnoSetupPath=%*
+SET "InnoSetupPath=%*"
 EXIT /B
 
 
-:SUBMSG
-ECHO. & ECHO ______________________________
-ECHO [%~1] %~2
-ECHO ______________________________ & ECHO.
-IF /I "%~1" == "ERROR" (
-  PAUSE
-  EXIT
-) ELSE (
-  EXIT /B
-)
+:EndWithError
+Title Compiling PerfmonBar [ERROR]
+ECHO. & ECHO.
+ECHO **ERROR: Build failed and aborted!**
+PAUSE
+ENDLOCAL
+EXIT
